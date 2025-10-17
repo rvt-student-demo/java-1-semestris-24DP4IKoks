4ebe2c47c2fcc4805bb3d62b7059b539a4b8cd0d
24DP4IKoks <A240848IK@rvt.lv>
2025-10-17 12:01:49 +0300

    Once Upon a Time

---
+++ src/main/java/rvt/OnceUponaTime.java
@@ -0,0 +1,9 @@
+package rvt;
+
+public class OnceUponaTime {
+    public static void main(String[] args) {
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
+    }
+}
